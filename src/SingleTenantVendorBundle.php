c7c5fa5c282b37f3196b0a7271a4260af66306d0
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2023-05-12 22:15:26 +0200

    add extension

--- src/SingleTenantVendorBundle.php
+++ src/SingleTenantVendorBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Ferdyrurka\SingleTenantVendor;
 
+use Ferdyrurka\SingleTenantVendor\DependencyInjection\SingleTenantVendorExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SingleTenantVendorBundle extends AbstractBundle
 {
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new SingleTenantVendorExtension();
+    }
 }
